b93855f90317e3bcaf6f927e73c305c576ea20f9
“thecdbob” <thecdbob@gmail.com>
2022-11-05 08:32:42 -0700

    Added back objects to js file, subject to change

--- studybuddy.js
+++ studybuddy.js
@@ -2,3 +2,17 @@ console.log("Testing Github Pages Hosting");
 function myFunction() {
     alert("Hello! I am an alert box!");
 }
+
+//default types list, implimentation may change in the future
+let types_array = [
+    "Assignment",
+    "Test",
+    "Lab",
+    "Exam"
+]
+
+//default group object implimentation may change in the future to JSON object 
+let default_group_object = {
+    name: 'default',
+    colour: 'black' //maybe use a different value later
+}
