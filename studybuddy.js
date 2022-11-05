1f023ab2294f1594eaf2df37dd4ada846621e163
“thecdbob” <thecdbob@gmail.com>
2022-11-04 19:50:34 -0700

    setting up firebase settings

--- studybuddy.js
+++ studybuddy.js
@@ -1,3 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  //...
+};
+
+const app = initializeApp(firebaseConfig);
+
+
 console.log("Testing Github Pages Hosting");
 function myFunction() {
     alert("Hello! I am an alert box!");
